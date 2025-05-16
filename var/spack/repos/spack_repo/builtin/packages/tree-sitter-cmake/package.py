a97d05faf8d74c77a8a959443684feb3dd665a8a
Peter Doak (epd) <doakpw@ornl.gov>
2025-05-16 10:57:07 -0400

    initial tree-sitter-cmake package.py

---
+++ var/spack/repos/spack_repo/builtin/packages/tree-sitter-cmake/package.py
@@ -0,0 +1,18 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class TreeSitterCmake(CMakePackage):
+    """CMAKE parser for tree-sitter"""
+
+    url = "https://github.com/uhya/tree-sitter-cmake"
+    git = "git@github.com:uyha/tree-sitter-cmake.git"
+
+    license("MIT", checked_by="PDoakORNL")
+
+    version("master", branch="master")
+
+    depends_on("tree-sitter")
